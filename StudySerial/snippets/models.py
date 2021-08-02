b1a582f076bd29b5a7ed49af6145cbcaf2fb7178
studyhoroom <dlaghtjd4487@naver.com>
2021-08-02 23:53:49 +0900

    인증 및 권한 시작

--- StudySerial/snippets/models.py
+++ StudySerial/snippets/models.py
@@ -2,8 +2,11 @@
 # Snippet에 대한 DB TABLE 모델 정의 
  
 from django.db import models
-from pygments.lexers import get_all_lexers
+from pygments.lexers import get_all_lexers, get_lexer_by_name
 from pygments.styles import get_all_styles
+from pygments.formatters.html import HtmlFormatter
+from pygments import highlight
+
 
 # (21.07.29) 어떤 내용인지 공부는 필요 
 # 간결한 코드 작성 법 참고 필요 
@@ -12,6 +15,8 @@ LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
 STYLE_CHOICES = sorted([(item,item) for item in get_all_styles()])
 
 class Snippet(models.Model):
+    owner = models.ForeignKey('auth.User',related_name='snippets', on_delete=models.CASCADE)
+    highlighted = models.TextField()
     created = models.DateField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
     code = models.TextField()
@@ -21,4 +26,14 @@ class Snippet(models.Model):
 
     class Meta:
         ordering = ['created']
+
+    # save 처리하는 함수에 대해서 더 공부가 필요 
+    def save(self, *args, **kwargs):
+        lexer = get_lexer_by_name(self.language)
+        linenos = 'table' if self.linenos else False
+        options = {'title' : self.title} if self.title else {}
+        formatter = HtmlFormatter(style=self.style, linenos=linenos,
+                                    full=True, **options)
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super(Snippet, self).save(*args, **kwargs)
 
